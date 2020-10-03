4d49d9f5da0b052d7c301337aa44135f715d6bcb
unknown <ankurjyotidas171@gmail.com>
2020-10-03 17:17:47 +0530

    test case

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,15 @@
 import React, {Component, useState} from "react";
 import '../styles/App.css';
+import Description from "./Description";
+import Name from "./Name";
 
 class App extends Component {
     render() {
 
         return(
             <>
-               
+               <Name/>
+               <Description/>
             </>
         )
     }
