1bac52638a9be6c147d7c08dc7f3bfd0761b3919
Ankurjyoti Das <68739425+AnkurJDas171@users.noreply.github.com>
2020-10-20 18:11:36 +0530

    Changes

---
+++ src/components/Description.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function Description() {
+  return (
+    <div data-ns-test attributes="project-description">
+      <p>
+        Its an ecommerce site where customers can buy pre-painted painting or
+        can order a custom made painting for them. Sellers (or the artists) can
+        upload there pre-paintings details in there profile or can take orders
+        for custom paintings.
+      </p>
+    </div>
+  );
+}
---
+++ src/components/Name.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function Name(){
+
+  return(
+
+    <div data-ns-test="project-name">
+      <h1>Colore's Magic</h1>
+    </div>
+
+
+  );
+
+
+}
