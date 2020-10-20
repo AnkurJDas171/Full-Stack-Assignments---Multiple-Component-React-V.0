2cfbb32f36c78a48ced6bd1f0fd8b884c9e8a393
Ankurjyoti Das <68739425+AnkurJDas171@users.noreply.github.com>
2020-10-20 18:15:33 +0530

    Changes on description

--- src/components/Description.js
+++ src/components/Description.js
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function Description() {
   return (
-    <div data-ns-test attributes="project-description">
+    <div data-ns-test attributes="project-name">
       <p>
         Its an ecommerce site where customers can buy pre-painted painting or
         can order a custom made painting for them. Sellers (or the artists) can
