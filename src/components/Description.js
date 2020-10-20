a2b2c663bb4c83ba968711fb6724b0e7e3c45ba9
Ankurjyoti Das <68739425+AnkurJDas171@users.noreply.github.com>
2020-10-20 18:18:13 +0530

    changes

--- src/components/Description.js
+++ src/components/Description.js
@@ -3,12 +3,10 @@ import React from "react";
 export default function Description() {
   return (
     <div data-ns-test attributes="project-name">
-      <p>
-        Its an ecommerce site where customers can buy pre-painted painting or
-        can order a custom made painting for them. Sellers (or the artists) can
-        upload there pre-paintings details in there profile or can take orders
-        for custom paintings.
-      </p>
+      Its an ecommerce site where customers can buy pre-painted painting or can
+      order a custom made painting for them. Sellers (or the artists) can upload
+      there pre-paintings details in there profile or can take orders for custom
+      paintings.
     </div>
   );
 }
